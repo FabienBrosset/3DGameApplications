158385a5e3ca69b32444d713aa27b32e301a9bb3
gabin <gabin.warnier-de-wailly@epitech.eu>
2021-05-22 17:22:45 +0900

    +upgrade player controller jump bug

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,6 +16,7 @@ public class PlayerController : MonoBehaviour
     public LayerMask mask;
     public GameObject spawnPoint;
     bool m_Started;
+    bool onCollision;
 
     public SettingsData SettingsData;
 
@@ -25,7 +26,7 @@ public class PlayerController : MonoBehaviour
         bc = GetComponent<MeshCollider>();
         anim = GetComponent<Animator>();
         m_Started = true;
-
+        onCollision = false;
     }
 
     void Update()
@@ -41,8 +42,16 @@ public class PlayerController : MonoBehaviour
         {
             die();
         }
+        if (!grounded && onCollision) {
+            move = 0;
+        }
+        float fall = rb.velocity.y;
+        if (fall < 0.1f && fall > -0.1f)
+        {
+            fall = 0;
+        }
         walkingAnimation(move);
-        rb.velocity = new Vector2(move, rb.velocity.y);
+        rb.velocity = new Vector2(move, fall);
     }
 
     void FixedUpdate()
@@ -89,8 +98,8 @@ public class PlayerController : MonoBehaviour
         int i = 0;
         Vector3 pos = transform.position;
 
-        pos.y += 0.1f;
-        Collider[] hitColliders = Physics.OverlapBox(pos, transform.localScale / 3, Quaternion.identity, mask);
+        pos.y += 0.05f;
+        Collider[] hitColliders = Physics.OverlapBox(pos, new Vector3(0.05f, 0.2f, 0.2f), Quaternion.identity, mask);
 
         while (i < hitColliders.Length)
         {
@@ -109,14 +118,24 @@ public class PlayerController : MonoBehaviour
 
         Vector3 pos = transform.position;
 
-        pos.y += 0.1f;
+        pos.y += 0.05f;
         if (m_Started)
-            Gizmos.DrawWireCube(pos, transform.localScale / 3);
+            Gizmos.DrawWireCube(pos, new Vector3(0.05f, 0.2f, 0.2f));
     }
 
     private void die()
     {
         transform.localPosition = spawnPoint.transform.localPosition;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        onCollision = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        onCollision = false;
     }
 
     private void OnTriggerEnter(Collider other)
